ee52de19bee6110ce3128d9f89c8b50b18e05f39
sevvaleygul0 <56435142+sevvaleygul0@users.noreply.github.com>
2020-05-06 18:50:32 +0300

    added custom event on bottom demo button

--- src/components/SectionCta.js
+++ src/components/SectionCta.js
@@ -1,34 +1,58 @@
-import React from 'react';
-import _ from 'lodash';
+import React from "react";
+import _ from "lodash";
 
-import {htmlToReact, Link, safePrefix} from '../utils';
+import { htmlToReact, Link, safePrefix } from "../utils";
+import { trackCustomEvent } from "gatsby-plugin-google-analytics";
 
 export default class SectionCta extends React.Component {
-    render() {
-        return (
-            <section id={_.get(this.props, 'section.section_id')} className="block cta-block bg-accent outer">
-              <div className="inner-large">
-                <div className="grid">
-                  <div className="cell block-content">
-                    {_.get(this.props, 'section.title') && 
-                    <h2 className="block-title">{_.get(this.props, 'section.title')}</h2>
-                    }
-                    {_.get(this.props, 'section.subtitle') && 
-                    <p className="block-subtitle">
-                      {htmlToReact(_.get(this.props, 'section.subtitle'))}
-                    </p>
-                    }
-                  </div>
-                  {_.get(this.props, 'section.actions') && 
-                  <div className="cell block-buttons">
-                    {_.map(_.get(this.props, 'section.actions'), (action, action_idx) => (
-                    <Link key={action_idx} to={safePrefix(_.get(action, 'url'))} className="button white large">{_.get(action, 'label')}</Link>
-                    ))}
-                  </div>
-                  }
-                </div>
+  render() {
+    return (
+      <section
+        id={_.get(this.props, "section.section_id")}
+        className="block cta-block bg-accent outer"
+      >
+        <div className="inner-large">
+          <div className="grid">
+            <div className="cell block-content">
+              {_.get(this.props, "section.title") && (
+                <h2 className="block-title">
+                  {_.get(this.props, "section.title")}
+                </h2>
+              )}
+              {_.get(this.props, "section.subtitle") && (
+                <p className="block-subtitle">
+                  {htmlToReact(_.get(this.props, "section.subtitle"))}
+                </p>
+              )}
+            </div>
+            {_.get(this.props, "section.actions") && (
+              <div className="cell block-buttons">
+                {_.map(
+                  _.get(this.props, "section.actions"),
+                  (action, action_idx) => (
+                    <div
+                      onClick={trackCustomEvent({
+                        category: "Special Button",
+                        action: "Click",
+                        label: "Gatsby Plugin Example Campaign",
+                        value: 43
+                      })}
+                    >
+                      <Link
+                        key={action_idx}
+                        to={safePrefix(_.get(action, "url"))}
+                        className="button white large"
+                      >
+                        {_.get(action, "label")}
+                      </Link>
+                    </div>
+                  )
+                )}
               </div>
-            </section>
-        );
-    }
+            )}
+          </div>
+        </div>
+      </section>
+    );
+  }
 }

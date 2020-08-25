e16c8812b11b8d2b5f323c497fb8827691cc9a7c
sevval <sevvalleygull@gmail.com>
2020-08-25 14:07:54 +0300

    Tried for metadata

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,22 +16,23 @@ export default class Body extends React.Component {
               _.get(this.props, "pageContext.frontmatter.title") + " - "}
             {_.get(this.props, "pageContext.site.siteMetadata.title")}
           </title>
-          {/* <meta
+          <meta
             name="viewport"
             content="width=device-width, initialScale=1.0"
           />
-          <meta name="google" content="notranslate" />  */}
-
-          {/* <meta charSet="utf-8" />
           <meta
             name="description"
             content="Harita tabanlı saha iş takip ve ekip yönetim uygulaması. Saha ekiplerinize harita üzerinde iş atayın, çalışanlarınızın konumlarını takip edin, sahadan anlık haberdar olun. Çalışanlarınızın performansını ve işlerinizi analiz ederek veriminizi arttırın. Saha iş takip ve ekip yönetiminde en iyi yardımcınız Pintask ile istediğiniz belge ve bilgilere her zaman ulaşın."
           />
+          <meta name="google" content="notranslate" />
+
+          <meta charSet="utf-8" />
+
           <link
             href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i"
             rel="stylesheet"
           />
-          <link rel="stylesheet" href={safePrefix("assets/css/main.css")} /> */}
+          <link rel="stylesheet" href={safePrefix("assets/css/main.css")} />
         </Helmet>
         <div
           id="page"
